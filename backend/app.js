6e7c7eed5caa277bf36c22e995adf6f0c28c1757
abin733 <abin733@aucklanduni.ac.nz>
2024-04-07 16:15:32 +1200

    init mongodb branch

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,32 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workout')
+
+// express app
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
