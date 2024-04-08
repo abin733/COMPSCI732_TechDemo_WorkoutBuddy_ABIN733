9cef632e2c84c2351162e46d3994eec3e26e0eef
abin733 <abin733@aucklanduni.ac.nz>
2024-04-08 14:26:44 +1200

    css fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   useEffect(() => {
     const fetchWorkouts = async () => {
       try {
-        const response = await fetch('${process.env.API_URL}/api/workouts');
+        const response = await fetch('http://localhost:3000/api/workouts');
         const json = await response.json();
 
         if (response.ok) {
