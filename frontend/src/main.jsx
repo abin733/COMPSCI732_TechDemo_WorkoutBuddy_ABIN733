8aba6b3f5e1a1c96de21922bcb88534a0a835b0c
abin733 <abin733@aucklanduni.ac.nz>
2024-04-09 09:08:52 +1200

    Revert "themeprovider context in main.js"
    This reverts commit 2fbca9df3bbaaf457142b85f97f0e5eb9b18b012.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { WorkoutsContextProvider } from './context/WorkoutsContext';
-import { ThemeProvider } from './context/ThemeContext'; // Import the DarkModeProvider
+import { DarkModeProvider } from './context/DarkModeContext'; // Import the DarkModeProvider
 import 'tailwindcss/tailwind.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <WorkoutsContextProvider>
-      <ThemeProvider> {/* Add the DarkModeProvider */}
+      <DarkModeProvider> {/* Add the DarkModeProvider */}
         <App />
-      </ThemeProvider>
+      </DarkModeProvider>
     </WorkoutsContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
